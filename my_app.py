5351bce25e55067ad6a41c86218edf35753c7e75
Peace Ikhile <87088482+Peaceosareme@users.noreply.github.com>
2024-06-10 11:37:15 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -11,26 +11,26 @@ st.title('Monthly Income Prediction')
 
 Age=st.number_input('Age',0,150)
 
-Gender=st.selectbox('Gender', [' ','Male','Female'])
+Gender=st.selectbox('Gender', ['','Male','Female'])
 if Gender=='Male':
     Gender=0
 else:
     Gender=1
 
-car=st.selectbox('Own a car or truck',[' ','Yes','No'])
+car=st.selectbox('Own a car or truck',['','Yes','No'])
 
 if car=='Yes':
     car =1
 else:
     car=0
 
-insurance=st.selectbox('health insurance',[' ','Yes','No'])
+insurance=st.selectbox('health insurance',['','Yes','No'])
 if insurance=='Yes':
     insurance =0
 else:
     insurance=1
 
-Education=st.selectbox('Education',[' ','Primary or less','Secondary', 'Higher'])
+Education=st.selectbox('Education',['','Primary or less','Secondary', 'Higher'])
 
 if Education=='Primary or less':
     Education=0
@@ -43,7 +43,7 @@ columns=['Age','Gender','Own a car or truck','health insurance','Education']
 
 prediction_placeholder = st.empty()
 def predict():
-    if Age == 0 or Gender == ' ' or car == ' ' or insurance == ' ' or Education == ' ':
+    if Age == 0 or Gender == '' or car == '' or insurance == '' or Education == '':
         st.error('Please fill in all fields.')
     else:
         row = np.array([Age, Gender, car, insurance, Education])
