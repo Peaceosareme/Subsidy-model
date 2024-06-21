f62866fe4518e6410b96b059553320ea233e3f80
Peace Ikhile <87088482+Peaceosareme@users.noreply.github.com>
2024-06-21 15:32:28 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -4,10 +4,7 @@ import numpy as np
 import joblib
 import sklearn
 
-mycursor = mydb.cursor()
-conn=st.connection('mysql', type='sql')
-
-model = joblib.load("C:/Users/Peace Ikhile/subsidy model/model_gender1")
+model = joblib.load("model_gender1")
 
 st.title('Monthly Income Prediction')
 
@@ -55,47 +52,15 @@ else:
 columns = ['Age', 'Gender', 'Own a car or truck', 'health insurance', 'Education']
 prediction_placeholder = st.empty()
 
-def generate_unique_id():
-    while True:
-        user_id = ''
-        for _ in range(10):
-            if np.random.choice([0, 1]):
-                user_id += chr(np.random.randint(48, 58))  # Add a random digit
-            else:
-                user_id += chr(np.random.randint(65, 91))  # Add a random alphabet
-
-        # Check if the generated ID already exists in the database
-        mycursor.execute("SELECT user_id FROM tele_users WHERE user_id = %s", (user_id,))
-        result = mycursor.fetchone()
-
-        if not result:
-            return user_id 
-        
-        
-def predict(Name, DOB, Age, Gender, car, insurance, Education):
-    Age_int = int(Age)
-    Gender_int = int(Gender)
-    car_int = int(car)
-    insurance_int = int(insurance)
-    Education_int = int(Education)
-
-    row = np.array([Age_int, Gender_int, car_int, insurance_int, Education_int])
+def predict():
+    row = np.array([Age, Gender, car, insurance, Education])
     X = pd.DataFrame([row], columns=columns)
     prediction = model.predict(X)[0]
 
     if prediction == 0:
-        prediction='<50k'
         prediction_placeholder.success('Income probability: <50k')
     else:
-        prediction='>50k'
         prediction_placeholder.error('Income probability: >50k')
-    prediction_str = str(prediction)
-
-    sql = "INSERT INTO tele_users (user_id, Name, DOB, Age, Gender, car, insurance, Education, prediction) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    
-    user_id = generate_unique_id() 
-    val = (user_id, Name, DOB, Age_int, Gender_str, car_str, insurance_str, Education_str, prediction_str)
-    mycursor.execute(sql, val)
-    mydb.commit()
-
-trigger = st.button('Predict', on_click=lambda: predict(Name, DOB, Age, Gender, car, insurance, Education))
+    import time
+    time.sleep(10)
+trigger = st.button('Predict', on_click=predict)
